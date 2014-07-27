cda4ecf69296f25d52a48a902354c6d52919d7a5
Jane Mendonca <imbecilemendonca@users.noreply.github.com>
2014-07-28 00:00:01 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,36 +1,87 @@
 (function(){
 	var app = angular.module('janeM',[]);
-	app.controller('AboutMe',function(){
-		this.product = AboutMeData;
+
+	//about me
+	app.controller('AboutMeCtrl',['$scope',function($scope){
+		$scope.aboutMeData = {firstName: 'Jane',lastName: 'Mendonca',designation : [{title: 'Web Developer'},{title: 'Cartoonist'},{title: 'Illustrator'}]};
+	}]).directive('aboutme', function(){
+		return {
+			template: 	'<label class="fontsize48">{{aboutMeData.firstName +" "+ aboutMeData.lastName}}</label> '+
+						'<ul class="horizontal"><li ng-repeat="obj in aboutMeData.designation">{{obj.title}}</li></ul>'
+		};
+	});
+	 
+	//references
+	app.controller('ReferencesCtrl', ['$scope',function($scope){
+		$scope.references = {links: [{text: 'facebook',url: 'http://facebook.com/imbecile.art'},{text: 'linkedin',url: 'https://www.linkedin.com/pub/jane-mendonca/43/18a/ab2'},
+				{text: 'youtube',url: 'http://www.youtube.com/user/ZDwarf/videos'}]};
+		$scope.contact = {title:"Contact: ",info:"janemendonca@zomgzomg.com"};
+	}]).directive('ref', function(){
+		return {
+			template: 	'<ul class="fontsize30 horizontal textalignright"><li class="lsf" ng-repeat="obj in references.links"><a ng-href="{{obj.url}}">{{obj.text}}</a></li></ul>'+
+						'<br><label>{{contact.title+" "+contact.info}}</label>'
+		};
 	});
 
-	var AboutMeData = {
-		firstName: 'Jane',
-		lastName: 'Mendonca',
-		designation : [
-			{
-				title: 'Web Developer'
-			},
-			{
-				title: 'Cartoonist'
-			},
-			{
-				title: 'Illustrator'
-			}
-		],
-		links: [
-			{
-				text: 'facebook',
-				url: 'http://facebook.com/imbecile.art'
-			},
-			{
-				text: 'linkedin',
-				url: 'https://www.linkedin.com/pub/jane-mendonca/43/18a/ab2'
-			},
-			{
-				text: 'youtube',
-				url: 'http://www.youtube.com/user/ZDwarf/videos'
-			}
-		]
-	}
+	//menu options
+	app.controller('ContentCtrl', ['$scope',function($scope){
+		$scope.menu = {options:[{title: 'Snake/Slug', url:'code.template.html'},{title: 'Table Tennis', url:'tabletennis.template.html'},{title: 'Design', url:'design.template.html'},
+		{title: 'Resume', url:'resume.template.html'}]};
+		$scope.currentTab='code.template.html';		
+		$scope.selectMenu = function(mobj){
+			$scope.currentTab=mobj.url;
+		};
+		$scope.activeMenu = function(mobj){
+			return mobj == $scope.currentTab;
+		};
+
+		$scope.images = [
+			{	caption:"British Accents - The Adventures of Colin and Jeeves: Comic Strip", url:"images/colinjeeves.png",
+				about:"Jeeves and his pet Colin embark on exciting Adventures as they learn about various Workplace Concepts."},
+			{	caption:"Daenerys - Game of Thrones Bookmark: Front",url:"images/daenerysbookmarkfront.png",
+				about:"The front sides of 3 separate bookmarks in Colour, Sepia and Monochrome."},
+			{	caption:"Daenerys - Game of Thrones Bookmark: Back",url:"images/daenerysbookmarkback.png",
+				about:"The back sides of 3 separate bookmarks in Colour, Sepia and Monochrome."}, 
+			{	caption:"Pixelated: A Self Potrait",url:"images/jane8bit.png",
+				about:"Retro Me."},
+			{	caption:"Vectorised: A Self Potrait",url:"images/janevector.png",
+				about:"Vectorised Me!"}, 
+			{	caption:"Tottenham-Liverpool Infographic: Match Statistics",url:"images/totliv.jpg",
+				about:"Poster Design for The Top Corner"}, 
+			{	caption:"West Brom-Man City: Pre-Match Poster",url:"images/wbrommancity.jpg",
+				about:"Poster Design for The Top Corner"},
+			{	caption:"Arsenal vs Chelsea: Post-Match Poster",url:"images/chelars.jpg",
+				about:"Poster Design for The Top Corner"}, 
+			{	caption:"Digital Colour & Digital Inking (ONLY)",url:"images/facescolour.jpg",
+				about:"Drawn by the friend of a friend."},
+			{	caption:"A look into the iPad Theatre Menu: App Design",url:"images/ipadmenuapp.png",
+				about:"Just an insight into an app of 10 screens!"}];
+		$scope.currentIndex=0;
+		$scope.loadPreviousImage = function(){
+			$scope.currentIndex > 0 ? $scope.currentIndex-- : $scope.currentIndex = $scope.images.length - 1;
+		};
+		$scope.loadNextImage = function(){
+			$scope.currentIndex < $scope.images.length - 1 ? $scope.currentIndex++ : $scope.currentIndex = 0;
+		};
+		$scope.$watch('currentIndex', function() {
+		  $scope.images.forEach(function(image) {
+		    image.visible = false;
+		  });
+		 
+		  $scope.images[$scope.currentIndex].visible = true;
+		});
+	}]).directive('menuoptions',function(){
+		return {
+			template: '<nav><section><ul class="horizontal"><li ng-repeat="opt in menu.options" ng-class="{active: activeMenu(opt.url)}" ng-click="selectMenu(opt)">{{opt.title}}</li></ul></section></nav>'
+		};
+	}).directive('contentarea',function(){
+		return {
+			template: '<section ng-include="currentTab" class="contentArea"></section>'
+		}
+	}).directive('imageslider',function(){
+		return {
+			template: '<input value="back" type="button" class="lsf" ng-click="loadPreviousImage()"/><input value="next" type="button" class="lsf" ng-click="loadNextImage()"/>'+
+			'<figure ng-repeat="img in images" ng-show="img.visible"><img src="{{img.url}}"><figcaption class="fontweightbold">{{img.caption}}</figcaption><figcaption class="fontsize14">{{img.about}}</figcaption></figure>'
+		}
+	});
 }) ();
